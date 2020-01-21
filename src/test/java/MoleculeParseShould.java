596ec2798597089dfc64aa4aa954a8d9018333d8
adriana <adrianapenate98@gmail.com>
2020-01-21 15:46:03 +0000

    Red test - throw_an_exception_when_is_not_a_molecule

---
+++ src/test/java/MoleculeParseShould.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MoleculeParseShould {
+    @Test
+    public void throw_an_exception_when_is_not_a_molecule() {
+        assertThatIllegalArgumentException().isThrownBy(() -> MoleculeParse.parse("pie"));
+    }
+
+    private static class MoleculeParse {
+        public static Map<String, Integer> parse(String molecule) {
+            return null;
+        }
+    }
+}
